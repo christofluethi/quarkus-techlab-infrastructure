4ad0a26120a481f48d3d6d779e6c681baee6ddf3
cluethi <luethi@puzzle.ch>
2020-09-07 15:06:42 +0200

    artemis

--- src/main/shell/deploy.sh
+++ src/main/shell/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 MICROSERVICES="data-producer data-consumer"
 
+date
 for ms in $MICROSERVICES
 do
-  tkn pipeline start apply-and-build -r git-repo=data-consumer-repo -r image=$ms-image -p deployment-name=$ms -p docker-file=src/main/docker/Dockerfile.multistage.jvm -s pipeline
+  tkn pipeline start apply-and-build -r git-repo=$ms-image-repo -r image=$ms-image -p deployment-name=$ms -p docker-file=src/main/docker/Dockerfile.multistage.jvm -s pipeline
 done
 
 
